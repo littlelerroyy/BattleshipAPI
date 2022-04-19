90729812f462a4bdb9a2ae5adc8cb82c81fe6dac
littlelerroyy <raptorial_radiation@live.com.au>
2022-04-19 14:43:27 +1000

    Check to see if the location specified is free on the players board.

--- BattleshipAPI/Models/Player.cs
+++ BattleshipAPI/Models/Player.cs
@@ -5,5 +5,9 @@
         public string? Name { get; set; }
         public List<Ship>? Ships { get; set; }
         public List<Location>? LocationsStriked { get; set; }
+        public bool LocationisFree(uint PosX, uint PosY)
+        {
+            return !Ships.Any(Ship => Ship.Locations.Any(Location => Location.xAxis == PosX && Location.yAxis == PosY));
+        }
     }
 }
