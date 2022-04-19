ac243f8d27d089121d536955929508c2a73a1509
littlelerroyy <raptorial_radiation@live.com.au>
2022-04-19 18:46:47 +1000

    Add missing test tags

--- BattleshipAPI/Unit Test/UnitTest.cs
+++ BattleshipAPI/Unit Test/UnitTest.cs
@@ -12,23 +12,26 @@ namespace BattleshipAPI.Unit_Test
 
             Assert.AreEqual(GameSession.Player1.LocationisFree(1, 3), true);
         }
+
         [Test]
-        public static void VerifyLocationIsNotFree() 
+        public static void VerifyLocationIsNotFree()
         {
             var GameSession = GameSessionMock1();
 
             Assert.AreEqual(GameSession.Player1.LocationisFree(1, 1), false);
         }
 
-        public static void SuccessFullShipStrike() 
+        [Test]
+        public static void SuccessFullShipStrike()
         {
             var GameSession = GameSessionMock1();
 
-            Assert.AreEqual(GameSession.Player1.StrikePlayer(1,1), GameSession.Player1.Ships[0]);
+            Assert.AreEqual(GameSession.Player1.StrikePlayer(1, 1), GameSession.Player1.Ships[0]);
             Assert.AreEqual(GameSession.Player1.StrikePlayer(2, 3), GameSession.Player1.Ships[1]);
 
         }
 
+        [Test]
         public static void UnSuccessFullShipStrike()
         {
             var GameSession = GameSessionMock1();
@@ -38,6 +41,7 @@ namespace BattleshipAPI.Unit_Test
 
         }
 
+        [Test]
         public static void ShipHasBeenDestroyed()
         {
             var GameSession = GameSessionMock1();
@@ -46,7 +50,6 @@ namespace BattleshipAPI.Unit_Test
             Assert.AreEqual(GameSession.Player1.StrikePlayer(2, 5), null);
 
         }
-
 
         private static GameSession GameSessionMock1()
         {
