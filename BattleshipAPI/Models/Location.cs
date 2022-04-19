9023cb4adc494bbcb1285b2aab594dee7346efaa
littlelerroyy <raptorial_radiation@live.com.au>
2022-04-19 13:29:20 +1000

    location to uint

--- BattleshipAPI/Models/Location.cs
+++ BattleshipAPI/Models/Location.cs
@@ -2,9 +2,9 @@
 {
     public class Location
     {
-        public int xAxis { get; set; }
+        public uint xAxis { get; set; }
 
-        public int yAxis { get; set; }
+        public uint yAxis { get; set; }
 
         public bool TargetDestroyed { get; set; }
     }
