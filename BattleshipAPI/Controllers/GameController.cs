b34ab23b74e37724d93683e992e0706aa978834d
littlelerroyy <raptorial_radiation@live.com.au>
2022-04-19 14:30:37 +1000

    Change Declare BoardSize to SetupGame and initialise Player 1 Properties. Player 2 wont be used for this task

--- BattleshipAPI/Controllers/GameController.cs
+++ BattleshipAPI/Controllers/GameController.cs
@@ -9,7 +9,7 @@ namespace BattleshipAPI.Controllers
     {
         [Route("/[controller]/[action]")]
         [HttpGet]
-        public IActionResult DeclareBoardSize([FromServices] GameSession GameSession, string SizeX, string SizeY)
+        public IActionResult SetupGame([FromServices] GameSession GameSession, string SizeX, string SizeY)
         {
             //Make sure the sizing that comes back is a positive number
             //Make Sure the sizing is greater than 3
@@ -17,14 +17,21 @@ namespace BattleshipAPI.Controllers
             {
                 GameSession.GridSizeX = uint.Parse(SizeX);
                 GameSession.GridSizeY = uint.Parse(SizeY);
-                if (GameSession.GridSizeX < 3 || GameSession.GridSizeY < 3) 
+                if (GameSession.GridSizeX < 3 || GameSession.GridSizeY < 3)
                 {
                     throw new Exception();
                 }
             }
-            catch {                
-                return BadRequest(new {Error="Please Enter Positive Numbers Only that are 3 or greater."});
+            catch
+            {
+                return BadRequest(new { Error = "Please Enter Positive Numbers Only that are 3 or greater." });
             }
+
+            //Create new Player and Ships
+            GameSession.Player1 = new Player();
+            GameSession.Player1.Ships = new List<Ship>();
+            GameSession.Player1.LocationsStriked = new List<Location>();
+
             return Ok();
         }
     }
