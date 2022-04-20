4e03915003d1b7d3f2988b86c7eb51d3a7342afc
littlelerroyy <raptorial_radiation@live.com.au>
2022-04-20 15:52:08 +1000

    Add swagger to release enviroment too

--- BattleshipAPI/Program.cs
+++ BattleshipAPI/Program.cs
@@ -13,12 +13,8 @@ builder.Services.AddSingleton<GameSession>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
